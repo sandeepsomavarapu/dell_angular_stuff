040879eec8f62ad77e7e126350e4efe0f6fc9db4
sandeep <sandeepsomavarapu535@gmail.com>
2023-07-12 15:01:53 +0530

    Done With CheckBoxWith FormArray

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'register',
@@ -7,11 +7,35 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  
   myForm:FormGroup;
         constructor(private fb:FormBuilder)
         {
 
         }
+Data:Array<any>=[
+{name:'Telugu',value:'telugu'},
+{name:'Hindi',value:'hindi'},
+{name:'English',value:'english'},
+{name:'Tamil',value:'tamil'},
+{name:'Kannada',value:'kannda'},
+];
+
+onCheckboxChange(e) {
+  const lang: FormArray = this.myForm.get('lang') as FormArray;
+  if (e.target.checked) {
+    lang.push(new FormControl(e.target.value));
+  } else {
+    let i: number = 0;
+    lang.controls.forEach((item: FormControl) => {
+      if (item.value == e.target.value) {
+        lang.removeAt(i);
+        return;
+      }
+      i++;
+    });
+  }
+}
   ngOnInit()
   {
     this.myForm=this.fb.group({
@@ -19,6 +43,7 @@ export class RegisterComponent {
       password:['',[Validators.required,Validators.minLength(6),Validators.maxLength(10)]],
       email:['',[Validators.required,Validators.email]],
       cnum:['', [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      lang:this.fb.array([],Validators.required),
       remember:['',[Validators.required]]
     })
   }
@@ -31,7 +56,7 @@ export class RegisterComponent {
     console.log(form.value.email);
     console.log(form.value.cnum);
     console.log(form.value.remember);
-    
+    console.log(form.value.lang);
   }
 
 }
